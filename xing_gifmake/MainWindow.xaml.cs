98786b86cb65d0cb2320e3dc9888caecef4e7061
Berumotto <732456503@qq.com>
2018-03-08 16:28:54 +0800

    第一个版本

---
+++ xing_gifmake/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace xing_gifmake
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //获取当前应用程序启动目录
+        string starpath = System.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+
+        //gif保存路径
+        string gifsavepath = "";
+
+        Run run = new Run();
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            
+            run.RunedEvent += RunedEvent;
+
+        }
+
+        #region 指令运行完成事件监听
+        private void RunedEvent(object rid)
+        {
+            if (rid.ToString() == "ass")
+            {
+                
+                string assstr = File.ReadAllText(starpath + "\\ffmpeg\\wsyw.ass");
+                //因为输出图片尺寸过小所以这里调整字幕字体大小，从16>30，直接用简单粗暴的文本替换实现。
+                assstr = assstr.Replace(",16,", ",30,");
+                File.WriteAllText(starpath + "\\ffmpeg\\wsyw.ass", assstr);
+
+                //将ass字幕嵌入视频，这里需要等待2分钟左右的时间才能完成字幕嵌入，不知道为何。
+                string Parameters = String.Format("-i " + starpath + "\\ffmpeg\\wsyw.mp4 -vf ass=wsyw.ass -y " + starpath + "\\ffmpeg\\wsyw_cache.mp4");
+
+                run.Execute(Parameters, "qr");
+
+            }
+            else if (rid.ToString() == "qr")
+            {
+
+
+                //嵌入字幕完成，导出gif
+                string Parameters = String.Format(" -i " + starpath + "\\ffmpeg\\wsyw_cache.mp4 -y -s 406x224 " + gifsavepath);
+
+                run.Execute(Parameters, "gif");
+
+            }
+            else
+            {
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+
+                    makebtn.IsEnabled = true;
+
+                    makebtn.Content = "生成GIF";
+
+                    MessageBox.Show("久等，生成GIF完成了~");
+
+                }));
+            }
+        }
+        #endregion
+
+        #region 生成GIF按钮点击
+        private void makebtn_Click(object sender, RoutedEventArgs e)
+        {
+            MakeSrt();
+
+            makebtn.IsEnabled = false;
+
+            makebtn.Content = "正在操作，请耐心等待...";
+
+          
+        }
+        #endregion
+
+        #region 生成台词（字幕文件）
+        //关于srt字幕可以参阅资料 https://jingyan.baidu.com/article/ce09321b7b75042bff858f0e.html
+        void MakeSrt()
+        {
+            string srt = "1\r\n00:00:00.970 --> 00:00:01.500\r\n" + l1.Text + "\r\n\r\n";
+            srt += "2\r\n00:00:03.110 --> 00:00:04.390\r\n" + l2.Text + "\r\n\r\n";
+            srt += "3\r\n00:00:05.180 --> 00:00:07.260\r\n" + l3.Text + "\r\n\r\n";
+            srt += "4\r\n00:00:07.260 --> 00:00:09.910\r\n" + l4.Text + "\r\n\r\n";
+            srt += "5\r\n00:00:10.000 --> 00:00:11.260\r\n" + l5.Text + "\r\n\r\n";
+            srt += "6\r\n00:00:11.630 --> 00:00:12.700\r\n" + l6.Text + "\r\n\r\n";
+            srt += "7\r\n00:00:13.610 --> 00:00:16.010\r\n" + l7.Text + "\r\n\r\n";
+            srt += "8\r\n00:00:18.080 --> 00:00:19.600\r\n" + l8.Text + "\r\n\r\n";
+
+            File.WriteAllText(starpath + "\\ffmpeg\\wsyw.srt", srt);
+
+            //转为ass字幕
+            string Parameters1 = String.Format("-i " + starpath + "\\ffmpeg\\wsyw.srt -y " + starpath + "\\ffmpeg\\wsyw.ass");
+            run.Execute(Parameters1, "ass");
+
+        }
+        #endregion
+
+        #region 选择保存路径按钮点击
+        private void savepath_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "GIF动态图|*.gif";
+            if (sfd.ShowDialog() == true)
+            {
+
+                savepath.Text = sfd.FileName;
+                gifsavepath = savepath.Text;
+            }
+
+        }
+        #endregion
+    }
+}
---
+++ xing_gifmake/Run.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace xing_gifmake
+{
+    public class Run
+    {
+
+        //获取当前应用程序启动目录
+        string starpath = System.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+
+        #region 委托事件，用于通知操作完成
+        public delegate void Handle(object rid);
+        public event Handle RunedEvent;
+        private void Event_(object rid)
+        {
+
+            RunedEvent?.Invoke(rid);
+
+        }
+        #endregion
+
+        #region 执行指令
+        public void Execute(string p, object rid)
+        {
+
+
+            Thread thread = new Thread(new ThreadStart(() =>
+            {
+                RunProcess(p, rid);
+                System.Windows.Threading.Dispatcher.Run();
+            }));
+
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+       
+
+        void RunProcess(string Parameters, object rid)
+        {
+
+            //设置要启动的ffmpeg程序完整路径
+            var oInfo = new ProcessStartInfo(starpath + "\\ffmpeg\\ffmpeg.exe", Parameters);
+
+            //[必须]设置启动进程的工作目录，不然后面的srt字幕嵌入指令会找不到srt字幕文件的哦，然后srt字幕自然就是放在这个目录内
+            oInfo.WorkingDirectory = starpath + "\\ffmpeg";
+
+            oInfo.UseShellExecute = false;//获取或设置一个值指示是否使用操作系统shell启动过程。
+
+            oInfo.CreateNoWindow = true;//获取或设置一个值指示是否开始在一个新的窗口过程。
+
+            oInfo.RedirectStandardOutput = true;//获取或设置指示是否将应用程序的文本输出写入 Process.StandardOutput 流中的值。
+
+            oInfo.RedirectStandardError = true;//获取或设置指示是否将应用程序的错误输出写入 Process.StandardError 流中的值。
+
+            oInfo.RedirectStandardInput = true;//获取或设置一个值,指出是否输入读取应用程序的过程。StandardInput流。
+
+
+            var proc = Process.Start(oInfo);
+
+            proc.EnableRaisingEvents = true;
+
+            //proc.Exited += new EventHandler(Proc_Exited);
+            proc.Exited += delegate (Object o, EventArgs e) { Proc_Exited(rid, o, e); };
+
+            proc.BeginErrorReadLine();
+
+            proc.WaitForExit();
+
+            proc.Close();//关闭进程
+
+            proc.Dispose();//释放资源
+
+
+        }
+        #endregion
+
+        #region 操作完成，进程退出
+        void Proc_Exited(object RID, object sender, EventArgs e)
+        {
+
+            //操作完成
+            Event_(RID);
+        }
+        #endregion
+
+    }
+}
